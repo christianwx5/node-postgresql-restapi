151ceb594c15cc4ce5fcfa25b5bf5d28f6130e09
Christian Di Santo <disantochistian@gmail.com>
2021-09-08 19:34:48 +0200

    conect sql

--- src/controllers/index.controllers.js
+++ src/controllers/index.controllers.js
@@ -1,17 +1,17 @@
 const { Pool } = require('pg');
 
 const pool = new Pool ({
-    host:'localhost',
-    user: 'postgres',
-    password: '12345',
-    database: 'firstapi',
+    host:'ec2-3-230-61-252.compute-1.amazonaws.com',
+    user: 'ncngrlakkkqqdc',
+    password: 'c841fdf8d2cf596acd155604b38e381dbe5f9b24db623b4567e9b335d7cc76d6',
+    database: 'd1mfa9vvp520pf',
     port: '5432'
 
 })
 
 const getUser = async (req,res)=>{
     console.log("mensaje antes del colappso");
-    const response = await pool.query('SELECT * FROM users');
+    const response = await pool.query('SELECT * FROM alumnos');
     console.log(response.rows);
     res.send('users 2 hola');
 }
